84a54623e7fc48572c1350dd6ed5f023913ecbca
Sasan Poor <sasan.pt.tec@gmail.com>
2023-05-02 23:03:02 +0430

    does it work

--- lib/db.tsx
+++ lib/db.tsx
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-
+    "mongodb+srv://Unow3n2:Random110@cluster0.t426ynn.mongodb.net/DataBase?retryWrites=true&w=majority"
   );
   return client;
 }
