6b006c48dad7bf297411e11f2bec38c6596b5de9
Sasan Poor <sasan.pt.tec@gmail.com>
2023-05-02 23:24:13 +0430

    test2

--- lib/db.tsx
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://Unow3n2:Random110@cluster0.t426ynn.mongodb.net/DataBase?retryWrites=true&w=majority"
-  );
-  return client;
-}
